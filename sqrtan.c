c7fda45448771f91a1922d7f2149e62f2a5482cd
miguelleitao <jml@isep.ipp.pt>
2018-10-01 15:44:48 +0100

    cmake file

--- sqrtan.c
+++ sqrtan.c
@@ -4,9 +4,22 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include "config.h"
+
 double myTan(double v) {
   double v_rad = v*M_PI/180.;
-  return tan(v_rad);
+  #ifdef HAVE_TAN
+    return tan(v_rad);
+  #else
+    double s, c;
+    #ifdef HAVE_SINCOS
+      sincos(v_rad, &s, &c);
+    #else
+      s = sin(v_rad);
+      c = cos(v_rad);
+    #endif
+    return s/c;
+  #endif
 }
 
 
