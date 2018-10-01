7fae8652a5ac7cbc2f5b0e21097a75099295e6aa
miguelleitao <jml@isep.ipp.pt>
2018-10-01 15:48:11 +0100

    config.h prototype

--- .hidden/run.sh
+++ .hidden/run.sh
@@ -34,4 +34,63 @@ ls
 
 
 
+# cMake
+git checkout master
+ls
+git checkout -b cmake
+cp .hidden/cmake/CMakeLists0.txt CMakeLists.txt
+cat CMakeLists.txt
+cmake .
+ls
+make
+ls
+./sqrtan 45
+
+cp .hidden/cmake/CMakeLists1.txt CMakeLists.txt
+cp .hidden/cmake/sqrtan1.c sqrtan.c
+cp .hidden/cmake/config.h.in .
+cat CMakeLists.txt
+cat sqrtan.c
+cmake .
+ls
+make
+ls
+./sqrtan 45
+
+cp .hidden/cmake/CMakeLists2.txt CMakeLists.txt
+#cp .hidden/cmake/sqrtan2.c sqrtan.c
+cat CMakeLists.txt
+cat sqrtan.c
+cmake .
+ls
+make
+ls
+./sqrtan 45
+
+
+cp .hidden/cmake/CMakeLists3.txt CMakeLists.txt
+cp .hidden/cmake/sqrtan3.c sqrtan.c
+cat CMakeLists.txt
+cat sqrtan.c
+cmake .
+ls
+make
+ls
+./sqrtan 45
+
+cp .hidden/cmake/CMakeLists4.txt CMakeLists.txt
+cp .hidden/cmake/sqrtan4.c sqrtan.c
+cat CMakeLists.txt
+cat sqrtan.c
+cmake .
+ls
+make
+ls
+./sqrtan 45
+
+git add CMakeLists.txt sqrtan.c
+git commit -m "cmake file"
+git push
+
+
 
